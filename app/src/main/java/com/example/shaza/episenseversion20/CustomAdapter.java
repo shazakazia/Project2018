43bab370c56555640a710024891391dc0d410404
shazakazia <37504062+shazakazia@users.noreply.github.com>
2018-04-19 17:58:21 +0400

    testing econtacts

--- app/src/main/java/com/example/shaza/episenseversion20/CustomAdapter.java
+++ app/src/main/java/com/example/shaza/episenseversion20/CustomAdapter.java
@@ -10,13 +10,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
  * Created by Shaza on 2/21/2018.
  */
 
 public class CustomAdapter extends ArrayAdapter<String> {
 
-    public CustomAdapter(@NonNull Context context, String[] myRecs) {
+    public CustomAdapter(@NonNull Context context, ArrayList<String> myRecs) {
         super(context, R.layout.record_item, myRecs);
     }
 
@@ -27,10 +29,13 @@ public class CustomAdapter extends ArrayAdapter<String> {
         View customView = myInflater.inflate(R.layout.record_item,parent,false);
 
         String singleitem = getItem(position);
-        TextView myText = (TextView) customView.findViewById(R.id.recordlabel);
+        TextView cName = (TextView) customView.findViewById(R.id.namelabel);
+/*
+        TextView cNum = (TextView) customView.findViewById(R.id.recordlabel);
+*/
         ImageView myImage = (ImageView) customView.findViewById(R.id.contactpic);
 
-        myText.setText(singleitem);
+        cName.setText(singleitem);
        // myImage.setImageResource(R.drawable.ic_menu_camera);
         return customView ;
     }
